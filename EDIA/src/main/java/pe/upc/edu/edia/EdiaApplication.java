c1968e7d319cb659d6b13fb90f69c16de3365e27
u202220034 <142704058+u202220034@users.noreply.github.com>
2025-04-19 18:49:17 -0500

    Add files via upload

---
+++ EDIA/src/main/java/pe/upc/edu/edia/EdiaApplication.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.edia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EdiaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EdiaApplication.class, args);
+    }
+
+}
---
+++ EDIA/src/test/java/pe/upc/edu/edia/EdiaApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.upc.edu.edia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdiaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
