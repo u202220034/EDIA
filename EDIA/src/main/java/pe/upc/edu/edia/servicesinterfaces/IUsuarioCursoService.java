7ba12bb4d520f4e94dacd672fec334a7d4bc1ffe
Ronald Yamil Cuellar Butrón <u202223827@upc.edu.pe>
2025-04-26 21:17:54 -0500

    Update IUsuarioCursoService.java

--- EDIA/src/main/java/pe/upc/edu/edia/servicesinterfaces/IUsuarioCursoService.java
+++ EDIA/src/main/java/pe/upc/edu/edia/servicesinterfaces/IUsuarioCursoService.java
@@ -4,6 +4,7 @@ import pe.upc.edu.edia.entities.UsuarioCurso;
 
 import java.util.List;
 
+
 public interface IUsuarioCursoService {
     public List<UsuarioCurso> list();
     public void insert(UsuarioCurso ucs);
