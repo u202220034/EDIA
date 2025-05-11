b1932675ed0aa7644713806b9c9a16cdc4ae3707
EBCT <u202220034@upc.edu.pe>
2025-05-11 15:15:57 -0500

    Update WebSecurityConfig.java

--- EDIA/src/main/java/pe/upc/edu/edia/security/WebSecurityConfig.java
+++ EDIA/src/main/java/pe/upc/edu/edia/security/WebSecurityConfig.java
@@ -56,15 +56,18 @@ public class WebSecurityConfig {
         auth.userDetailsService(jwtUserDetailsService).passwordEncoder(passwordEncoder());
     }
 
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         //Desde Spring Boot 3.1+
         httpSecurity
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req
-                        .requestMatchers(antMatcher("/Inicio_de_Sesion")).permitAll()
+                        .requestMatchers(antMatcher("/login")).permitAll()
+                        .requestMatchers(antMatcher("/desafiotemporal/**")).permitAll()
+                        .requestMatchers(antMatcher("/usuariosdesafios/**")).permitAll()
                         .requestMatchers(antMatcher("/usuarioscursos/**")).permitAll()
-                        .requestMatchers(AUTH_WHITELIST).permitAll()
+
                         .anyRequest().authenticated()
                 )
                 .httpBasic(Customizer.withDefaults())
@@ -74,12 +77,4 @@ public class WebSecurityConfig {
         httpSecurity.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
         return httpSecurity.build();
     }
-    private static final String[] AUTH_WHITELIST={
-            "/api/v1/auth/**",
-            "/v3/api-docs/**",
-            "/v3/api-docs.yaml",
-            "/swagger-ui/**",
-            "/swagger-ui.html"
-
-    };
 }
